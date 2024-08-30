6341b180bae93b510c6db23520c65f7efee46f1f
“Zeta <chazapi.zafeiria@gmail.com>
2024-08-30 14:49:55 +0300

    up to chapter 2 - functional program exc

--- longestLine.c
+++ longestLine.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAXLINE 1000
 
 int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
+char longestThanEighty[MAXLINE];
+void reverse(char s[], int p, int r);
+int x, p, r;
 
-main()
+int main()
 {
-    int len, max;
-    char line[MAXLINE], longest[MAXLINE];
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
 
     max = 0;
-    while ((len = getline(line, MAXLINE)) > 0)
+    while ((len = getLine(line, MAXLINE)) > 0)
     {
         if (len > max)
         {
@@ -19,10 +25,19 @@ main()
             copy(longest, line);
         }
     }
+
     if (max > 0)
     {
-        printf("%s", longest);
+        printf("\nLongest line and its length:\n");
+        printf("%s%d\n", longest, max);
+        printf("%s\n", longestThanEighty);
     }
+
+    char testString[] = "Hello, World!";
+    printf("Original string: %s\n", testString);
+    reverse(testString, 0, strlen(testString) - 1);
+    printf("Reversed string: %s\n", testString);
+
     return 0;
 }
 
@@ -30,17 +45,23 @@ int getLine(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != 'n'; ++i)
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
     {
         s[i] = c;
-        if (c == '\n')
-        {
-            s[i] = c;
-            ++i;
-        }
-        s[i] = '\0';
-        return i;
     }
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+
+    if (i >= 80)
+    {
+        copy(longestThanEighty, s); // Copy the line to longestThanEighty
+    }
+    return i;
 }
 
 void copy(char to[], char from[])
@@ -51,5 +72,16 @@ void copy(char to[], char from[])
     while ((to[i] = from[i]) != '\0')
     {
         ++i;
+    }
+}
+
+void reverse(char s[], int p, int r)
+{
+    if (p < r)
+    {
+        x = s[p];
+        s[p] = s[r];
+        s[r] = x;
+        reverse(s, p + 1, r - 1);
     }
 }
