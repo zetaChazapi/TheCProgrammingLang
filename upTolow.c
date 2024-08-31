6f90fd000d46260b2d07a381fe03beefb57bb50d
“Zeta <chazapi.zafeiria@gmail.com>
2024-08-31 19:49:09 +0300

    commit

--- upTolow.c
+++ upTolow.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+// get char one by one
+// find its lowercase equivelant
+// put c that equivalent
+
+int main()
+{
+    int current;
+
+    while ((current = getchar()) != EOF)
+    {
+        if (current >= 'A' && current <= 'B')
+        {
+            char lower = current + ('a' - 'A');
+            putchar(lower);
+        }
+        else
+        {
+            putchar(current);
+        }
+    }
+}
