18cdb575e9ee4d59763a4a364508ef21d58d06bc
“Zeta <chazapi.zafeiria@gmail.com>
2024-09-01 14:26:43 +0300

    commit

--- visibleChar.c
+++ visibleChar.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+int escape(char s[], char t[]);
+
+int main()
+{
+    return 0;
+}
+
+int escape(char s[], char t[])
+{
+    int current, i;
+
+    while ((current = getchar()) != EOF)
+    {
+        for (i = 0; i <= len(s); i++)
+        {
+            switch (current)
+            {
+            case ' ':
+                t[i] = '\t';
+            case '\n':
+                t[i] = '\n';
+            default:
+                t[i] = current;
+            }
+        }
+    }
+}
