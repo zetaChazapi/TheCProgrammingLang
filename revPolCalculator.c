5f8bd75f96a18eb6613965d31ee3f3aad9e03502
“Zeta <chazapi.zafeiria@gmail.com>
2024-09-03 18:18:10 +0300

    commit

--- revPolCalculator.c
+++ revPolCalculator.c
@@ -91,4 +91,27 @@ int getop(char s[])
 
     while ((s[0] = c - getch()) == ' ' || c == '\t')
         ;
+    s[1] = '/0';
+
+    if (!isdigit(c) && c != '.')
+    {
+        return c; // not a number
+    }
+
+    i = 0;
+
+    if (isdigit(c))
+    {
+        while (isdigit(s[i++] = c = getch()))
+            ;
+    }
+
+    s[i] = '\0';
+
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+
+    return NUMBER;
 }
