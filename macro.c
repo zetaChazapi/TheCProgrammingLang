0b0840e1192c6f11fdcb329fa8f064ebf897cd0d
“Zeta <chazapi.zafeiria@gmail.com>
2024-09-07 08:08:54 +0300

    commit

--- macro.c
+++ macro.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
-#define swap(t, x, y)
+#define swap(t, x, y) \
+    do                \
+    {                 \
+        t temp = x;   \
+        x = y;        \
+        y = temp;     \
+    } while (0)
+
+int main()
+{
+    int a = 5, b = 10;
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(int, a, b);
+
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
